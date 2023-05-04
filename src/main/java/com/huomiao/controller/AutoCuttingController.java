336952f668ce93af3a2844a796b4cac7a80eee4b
ZXYANG <740444603@qq.com>
2023-05-04 17:34:14 +0800

    配置上传

--- src/main/java/com/huomiao/controller/AutoCuttingController.java
+++ src/main/java/com/huomiao/controller/AutoCuttingController.java
@@ -1,5 +1,6 @@
 package com.huomiao.controller;
 
+import cn.hutool.core.exceptions.ExceptionUtil;
 import com.alibaba.fastjson.JSONObject;
 import com.huomiao.aspect.License;
 import com.huomiao.config.ConfigInit;
@@ -7,12 +8,13 @@ import com.huomiao.service.AutoCutService;
 import com.huomiao.utils.HttpClientUtils;
 import com.huomiao.vo.RequestBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/cut")
@@ -45,10 +47,24 @@ public class AutoCuttingController {
     public RequestBean<String> config() throws IOException {
         boolean init = configInit.initUpdate();
         if (init){
-            httpClientUtils.doGet(configInit.getVirApi()+"gc.php/?gc="+JSONObject.toJSONString(configInit));
+            pushConfig();
             return RequestBean.Success();
         }else {
             return RequestBean.Error();
+        }
+    }
+
+    public void pushConfig() throws FileNotFoundException {
+        String url = configInit.getVirApi()+"gc.php";
+        Map<String, File> fileMap = new HashMap<>();
+        String configPath = ResourceUtils.getURL("config.txt").getPath();
+        if (!configInit.isOsLinux()){
+            configPath = ResourceUtils.getURL("config.txt").getPath().substring(1);
+        }
+        fileMap.put("file",new File(configPath));
+        try {
+            String respond = httpClientUtils.uploadFile(url, null, null, fileMap);
+        }catch (Exception e){
         }
     }
 
